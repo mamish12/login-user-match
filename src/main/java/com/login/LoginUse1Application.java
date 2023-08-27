c31e6ab032fe4daf6eba38f0edb6f8cd0ffd3ab3
mamish12 <mku20sahu@gmail.com>
2023-08-27 15:10:36 +0530

    changes

--- src/main/java/com/login/LoginUse1Application.java
+++ src/main/java/com/login/LoginUse1Application.java
@@ -8,7 +8,7 @@ public class LoginUse1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginUse1Application.class, args);
-		System.out.println("hello world2");
+		System.out.println("hello world2 and 5");
 		System.out.println("git is created2");
 	}
 
