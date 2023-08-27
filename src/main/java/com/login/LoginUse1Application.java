1297c834466dacf589fc5c2483e5a9a6d92f1959
mamish12 <mku20sahu@gmail.com>
2023-08-27 14:05:28 +0530

    success

---
+++ src/main/java/com/login/LoginUse1Application.java
@@ -0,0 +1,15 @@
+package com.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginUse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginUse1Application.class, args);
+		System.out.println("hello world1");
+		System.out.println("git is created");
+	}
+
+}
